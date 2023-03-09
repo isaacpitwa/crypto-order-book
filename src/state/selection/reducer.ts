2564795800e196d13097495dc6c0d2e0d03e203f
Isaac Pitwa <isaacpitwa256@gmail.com>
2023-03-09 19:13:03 +0300

    Define Reducers for selection actions

---
+++ src/state/selection/reducer.ts
@@ -0,0 +1,22 @@
+
+import { SELECTION_MADE, BASE_SELECTION_MADE,QUOTE_SELECTION_MADE } from "./actions";
+import { Token } from '@/state/tokens/actions';
+
+type Action = {
+    type: string;
+    token: Token;
+}
+
+
+export default function reducer(state = {}, action: Action = {} as Action) {
+    switch (action.type) {
+      case SELECTION_MADE:
+    return { ...state, token: action.token };
+    case BASE_SELECTION_MADE:
+        return { ...state, baseToken: action.token };
+    case QUOTE_SELECTION_MADE:
+    return { ...state, quoteToken: action.token };
+    default:
+    return state;
+    }
+  }
