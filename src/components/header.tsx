d755ab6474da44e3c893c4729b1e1ba6bda29bea
Isaac Pitwa <isaacpitwa256@gmail.com>
2023-03-11 02:04:54 +0300

    Localization polishing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@ import {
   Box,
   Flex,
   Avatar,
-  Link,
   Button,
   Menu,
   MenuButton,
@@ -17,19 +16,30 @@ import {
   Center,
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTranslations } from 'next-intl';
 
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const {locale, locales, route} = useRouter();
+  const otherLocale = locales?.find((cur) => cur !== locale);
+  const t = useTranslations('Base');
+
+
  return (
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} w='100%'>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'} >
-          <Box>Crypto Order Book</Box>
+          <Box>{t('title')}</Box>
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
               <Button onClick={toggleColorMode}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
               </Button>
+              <Link href={route} locale={otherLocale}>
+                {otherLocale}
+              </Link>
             </Stack>
           </Flex>
         </Flex>
